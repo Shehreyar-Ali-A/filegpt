f91500f34584979e6be03bfa3aab643ed3a992c3
Shehreyar Ali <shehreyar1ali@gmail.com>
2024-04-29 16:54:04 +0500

    deployment related changes added

--- backend/filegpt_backend/filegpt_backend/settings.py
+++ backend/filegpt_backend/filegpt_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-gj0&2scqrr%n9onxw)(c*^e)&_mf(pyte+z5p%$=o*=khb(&-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
