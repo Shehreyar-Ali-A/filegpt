6666691c722c2e924d7bfea94d299ec6da735c52
Shehreyar Ali <shehreyar1ali@gmail.com>
2024-05-01 03:30:41 +0500

    backend changes

--- backend/filegpt_backend/filegpt_backend/settings.py
+++ backend/filegpt_backend/filegpt_backend/settings.py
@@ -58,8 +58,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000', 
+    'http://localhost:3030',
 ]
 
 
